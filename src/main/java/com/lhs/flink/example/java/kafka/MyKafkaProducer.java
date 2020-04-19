e32b4d97abadc3d13969cfbea6015636297892c9
lihuasong <lihuasong@ixianlai.com>
2020-04-19 20:38:14 +0800

    flink examples

---
+++ src/main/java/com/lhs/flink/example/java/kafka/MyKafkaProducer.java
@@ -0,0 +1,36 @@
+package com.lhs.flink.example.java.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @ClassNameMyKafkaProducer
+ * @Description
+ * @Author lihuasong
+ * @Date2020/4/19 10:51
+ * @Version V1.0
+ **/
+public class MyKafkaProducer {
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties properties = new Properties();
+
+        properties.put("bootstrap.servers","localhost:9092");
+        properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer producer = new KafkaProducer(properties);
+
+        while (true){
+            long current = System.currentTimeMillis();
+            ProducerRecord<String,String> record1 = new ProducerRecord<>("flink","k1,"+current);
+            producer.send(record1);
+            System.out.println("k1,"+current);
+            Thread.sleep(1000L);
+        }
+
+    }
+
+}
