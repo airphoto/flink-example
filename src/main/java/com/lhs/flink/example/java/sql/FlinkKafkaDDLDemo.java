9436127c81d6bc0e01adf2c2e0f963ee607a8737
lihuasong <lihuasong@ixianlai.com>
2020-04-20 21:01:23 +0800

    flink table

---
+++ src/main/java/com/lhs/flink/example/java/sql/FlinkKafkaDDLDemo.java
@@ -0,0 +1,55 @@
+package com.lhs.flink.example.java.sql;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * @author lihuasong
+ * @description 描述
+ * @create 2020/4/20
+ **/
+public class FlinkKafkaDDLDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        EnvironmentSettings environmentSettings = EnvironmentSettings.newInstance()
+                .useBlinkPlanner()
+                .inStreamingMode()
+                .build();
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(environment, environmentSettings);
+
+        String createTable = "create table wordCount(name varchar,score bigint) " +
+                "WITH('connector.type' = 'kafka', " +
+                "'connector.version' = '10'," +
+                "'connector.topic' = 'flink', " +
+                "'connector.property-version' = '1', " +
+                "'connector.startup-mode' = 'latest-offset'," +
+                "'connector.properties.0.key' = 'bootstrap.servers'," +
+                "'connector.properties.0.value' = 'localhost:9092'," +
+                "'format.derive-schema' = 'true'," +
+                "'update-mode' = 'append'," +
+                "'format.type' = 'json')";
+
+        tableEnv.sqlUpdate(createTable);
+        for (String t:tableEnv.listTables()){
+            System.out.println(t);
+        }
+        String query = "select name,sum(score) as score from wordCount group by name";
+
+        Table sqlQuery = tableEnv.sqlQuery(query);
+
+        tableEnv.toAppendStream(sqlQuery, Row.class).print();
+//
+        tableEnv.execute("table env");
+
+    }
+
+}
